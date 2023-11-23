2a17d2c5d5480b8e10539450150ef95ddcd4b105
noravitkai <norvit01@easv365.dk>
2023-11-23 17:32:52 +0100

    Add a Services Section
    Implemented a Services Section on the Home Page with dynamic content displayed on cards for each service in the loop of the Service custom post type. The section includes icons, names, descriptions, and buttons, providing a visually appealing and responsive user experience.

--- home-page.php
+++ home-page.php
@@ -8,25 +8,81 @@ Template Name: Home
 
 <main>
 
-    <!-- Hero Section -->
-    <div class="relative bg-lightpink">
-        <div class="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
-            <div class="px-6 py-10 sm:py-32 lg:col-span-7 lg:px-0 lg:py-48 xl:col-span-6 relative">
-                <div class="mx-auto max-w-2xl lg:mx-0">
-                    <h1 class="text-6xl font-secondary font-bold tracking-tight text-whitesmoke sm:text-8xl opacity-50 absolute z-10 lg:top-40 md:top-24 top-12"><?php the_field('hero_heading_1') ?></h1>
-                    <h2 class="mt-12 text-3xl font-primary font-bold tracking-tight text-black sm:mt-10 sm:text-5xl relative z-20"><?php the_field('hero_heading_2') ?></h2>
-                    <button class="mt-10 gap-x-6 group relative overflow-hidden px-3.5 py-2.5 text-sm sm:text-base font-primary font-semibold text-black shadow-sm border-solid border-[0.075rem] border-black">
-                        <div class="absolute inset-0 w-3 bg-darkpink transition-all duration-[250ms] ease-out group-hover:w-full"></div>
-                        <span class="relative group-hover:text-lightpink"><?php the_field('hero_button') ?></span>
-                    </button>
-                </div>
-            </div>
-            <div class="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
-                <?php $hero_image = get_field('hero_image'); ?>
-                <img class="aspect-[3/2] w-full object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full" src="<?php echo esc_url($hero_image['url']); ?>" alt="<?php echo esc_attr($hero_image['alt']); ?>">  
+<!-- Hero Section -->
+<section class="relative bg-lightpink">
+    <div class="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-28 lg:px-8">
+        <div class="px-6 py-10 sm:py-32 lg:col-span-7 lg:px-0 lg:py-48 xl:col-span-6 relative flex items-center justify-center">
+            <div class="mx-auto max-w-2xl lg:mx-0 flex flex-col items-center text-center">
+                <h1 class="text-6xl font-secondary font-bold tracking-tight text-whitesmoke sm:text-8xl opacity-50 absolute z-10 lg:top-40 md:top-24 top-12 w-full"><?php the_field('hero_heading_1') ?></h1>
+                <h2 class="mt-12 text-3xl font-primary font-bold tracking-tight text-black sm:mt-10 sm:text-5xl relative z-20"><?php the_field('hero_heading_2') ?></h2>
+                <button class="mt-10 gap-x-6 group relative overflow-hidden px-3.5 py-2.5 text-sm sm:text-base font-primary font-semibold text-black shadow-sm border-solid border-[0.075rem] border-black">
+                    <div class="absolute inset-0 w-3 bg-darkpink transition-all duration-[250ms] ease-out group-hover:w-full"></div>
+                    <span class="relative group-hover:text-lightpink"><?php the_field('hero_button') ?></span>
+                </button>
             </div>
         </div>
+        <div class="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
+            <?php $hero_image = get_field('hero_image'); ?>
+            <img class="aspect-[3/2] w-full object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full" src="<?php echo esc_url($hero_image['url']); ?>" alt="<?php echo esc_attr($hero_image['alt']); ?>">  
+        </div>
     </div>
+</section>
+
+<!-- Services Section -->
+<section class="bg-whitesmoke py-10 sm:py-24 relative">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mb-16 flex justify-center">
+            <h2 class="text-6xl font-secondary font-bold tracking-tight text-lightpink sm:text-8xl opacity-50 absolute z-10 lg:top-16 md:top-16 top-12"><?php the_field('services_heading') ?></h2>
+            <h3 class="mt-12 text-2xl font-primary font-bold tracking-tight text-black sm:mt-10 sm:text-3xl relative z-20"><?php the_field('services_subheading') ?></h3>
+        </div>
+
+        <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-10 gap-y-10 sm:gap-y-10 lg:mx-0 lg:max-w-none lg:grid-cols-2 relative">
+            <dl class="col-span-2 grid grid-cols-1 gap-x-8 gap-y-10 sm:grid-cols-2">
+
+                <?php
+                $args = array(
+                    'post_type' => 'service',
+                    'posts_per_page' => -1,
+                    'order' => 'ASC',
+                );
+
+                $services_query = new WP_Query($args);
+
+                if ($services_query->have_posts()) :
+                    while ($services_query->have_posts()) : $services_query->the_post();
+                ?>
+
+                        <div class="border-solid border-[0.075rem] border-black p-6 flex items-center shadow-sm hover:shadow-lg transition duration-300 ease-in-out transform hover:scale-105">
+                            <div class="mr-6 w-4/12">
+                                <?php $service_icon = get_field('service_icon'); ?>
+                                <?php if ($service_icon) : ?>
+                                    <img src="<?php echo esc_url($service_icon['url']); ?>" alt="<?php echo esc_attr($service_icon['alt']); ?>" class="max-w-full">
+                                <?php endif; ?>
+                            </div>
+                            <div class="w-8/12">
+                                <h4 class="text-sm sm:text-base font-primary font-semibold leading-7 tracking-tight text-black">
+                                    <?php the_field('service_name'); ?>
+                                </h4>
+                                <p class="mt-1 text-sm sm:text-base font-primary leading-7 tracking-tight text-black"><?php the_field('service_description'); ?></p>
+                                <button class="mt-6 gap-x-6 group relative overflow-hidden px-3.5 py-2.5 text-sm sm:text-base font-primary font-semibold text-black shadow-sm border-solid border-[0.075rem] border-black">
+                                    <div class="absolute inset-0 w-3 bg-darkpink transition-all duration-[250ms] ease-out group-hover:w-full"></div>
+                                    <span class="relative group-hover:text-whitesmoke"><?php the_field('service_button'); ?></span>
+                                </button>
+                            </div>
+                        </div>
+
+                    <?php
+                    endwhile;
+                    wp_reset_postdata();
+                else :
+                    echo 'Jelenleg nincs szolgáltatásunk.';
+                endif;
+                ?>
+
+            </dl>
+        </div>
+    </div>
+</section>
 
 </main>
 
